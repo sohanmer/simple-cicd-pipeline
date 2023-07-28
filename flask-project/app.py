621301ed53b15527009e565c993cc626e57200db
sohanmer <sohan1510@gmail.com>
2023-07-29 01:47:54 +0530

    updated .gitignore

--- flask-project/app.py
+++ flask-project/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Docker</h1>'
 
 if __name__ == "__main__":
     app.run()
