ccb42fc4159463efbf717960bbed3194644604cb
sohanmer <sohan1510@gmail.com>
2023-07-29 01:23:07 +0530

    fixed docker file

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World!!"
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run()
